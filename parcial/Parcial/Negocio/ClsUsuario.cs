38a18df0b97978cf98098bb6b3340abe7da0aba6
BrayanEAA <brayaneduardoarteaga@gmail.com>
2023-03-06 20:24:50 -0600

    negocio

--- parcial/Parcial/Negocio/ClsUsuario.cs
+++ parcial/Parcial/Negocio/ClsUsuario.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parcial.Entidades;
 
 namespace Parcial.Negocio
 {
-    internal class ClsUsuario
+    public class ClsUsuario
     {
+        public bool Acceso(string nombre, string contra)
+        {
+            // Verificar las credenciales del usuario
+            return nombre == "TUNOMBRE" && contra == "123";
+        }
+    }
+    public class ClsVenta
+    {
+        public void Cobro(Venta venta)
+        {
+            decimal total = venta.PrecioDelProducto * venta.CantidadDeProducto;
+
+            // Aplicar descuento si el total de la venta es mayor a $50
+            if (total > 50)
+            {
+                total -= 10;
+                Console.WriteLine($"Precio original: ${venta.PrecioDelProducto * venta.CantidadDeProducto}");
+                Console.WriteLine($"Descuento aplicado: $10");
+                Console.WriteLine($"Total a pagar: ${total}");
+            }
+            else
+            {
+                Console.WriteLine($"Total a pagar: ${total}");
+            }
+        }
     }
 }
