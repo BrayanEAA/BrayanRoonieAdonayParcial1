9e49561a29e8908342905a3350272262323443d1
BrayanEAA <brayaneduardoarteaga@gmail.com>
2023-03-06 20:54:53 -0600

    cambios

--- parcial/Parcial/Entidades/Usuario.cs
+++ parcial/Parcial/Entidades/Usuario.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Parcial.Entidades
 {
-    internal class Usuario
+    public class Usuario
     {
+        public string Nombre { get; set; }
+        public string Contra { get; set; }
     }
+
+    public class Venta
+    {
+        public int IdentificadorDeProducto { get; set; }
+        public string NombreDelProducto { get; set; }
+        public string DescripcionDelProducto { get; set; }
+        public decimal PrecioDelProducto { get; set; }
+        public int CantidadDeProducto { get; set; }
+    }
+
 }
